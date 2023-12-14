08b135d10e8540aa98f410e57783a94a2d2ac78c
sinans <svrsinan06@gmail.com>
2023-12-14 23:02:20 +0300

    fixed: blog create

--- backend/src/controllers/blog.js
+++ backend/src/controllers/blog.js
@@ -4,7 +4,17 @@ const Blog = require("../models/blog");
 
 module.exports = {
   list: async (req, res) => {
-    const data = await res.getModelList(Blog);
+    let filters = { status: "Published" };
+
+    if (req?.query?.author && req.user.username == req?.query?.author) {
+      filters = req.query;
+    }
+
+    if (req?.query?.author && !(req.user.username == req?.query?.author)) {
+      throw new Error("You can only see your own blog");
+    }
+
+    const data = await res.getModelList(Blog, filters, "category");
 
     res.status(200).send({
       error: false,
@@ -14,7 +24,9 @@ module.exports = {
   },
 
   create: async (req, res) => {
-    const data = await Blog.create(req.body);
+
+    //add user id to author
+    const data = await Blog.create({ ...req.body, author: req.user._id });
 
     res.status(201).send({
       error: false,
@@ -23,15 +35,17 @@ module.exports = {
   },
 
   read: async (req, res) => {
+    let data = await Blog.findOne({ _id: req.params.id });
 
-    let data = await Blog.findOne({ _id: req.params.id }); 
-
-    //Bloğu okuyan kişilerin id'lerinin uniqe olarak kaydedilmesi ve görüntülenme sayısının +1 yapılması
-    if(req.user._id && !data.visitors.includes(req.user._id)){
-      data.visitors.push(req.user._id.toString())   
-      data.views +=1;
-      await Blog.updateOne({ _id: req.params.id }, {$set : {visitors:data.visitors, views:data.views}})
-    }   
+    // save unique readers ids into visitors and inc views +1
+    if (req?.user._id && !data.visitors.includes(req?.user._id)) {
+      data.visitors.push(req?.user._id.toString());
+      data.views += 1;
+      await Blog.updateOne(
+        { _id: req?.params.id },
+        { $set: { visitors: data.visitors, views: data.views } }
+      );
+    }
 
     res.status(200).send({
       error: false,
@@ -40,22 +54,36 @@ module.exports = {
   },
 
   update: async (req, res) => {
-    
-    const theBlog = await Blog.findOne({ _id: req.params.id})
+    const theBlog = await Blog.findOne({ _id: req.params.id });
 
-    if(theBlog.author==req.user._id){
-      const data = await Blog.updateOne({ _id: req.params.id }, req.body);
+    //only blog owner update
+    if (theBlog.author == req?.user._id) {
+      const data = await Blog.updateOne({ _id: req?.params.id }, req.body);
+    } else {
+      throw new Error("You can't change the blog!");
     }
-    
+
     res.status(202).send({
       error: false,
       data,
-      new: await Blog.findOne({ _id: req.params.id }),
+      new: await Blog.findOne({ _id: req?.params.id }),
     });
   },
 
   delete: async (req, res) => {
-    const data = await Blog.deleteOne({ _id: req.params.id });
+    const theBlog = await Blog.findOne({ _id: req.params.id });
+
+    console.log(theBlog.author.toString(), req?.user._id.toString());
+
+    // Only the blog owner or admin can delete
+    if (
+      theBlog.author.toString() == req?.user._id.toString() ||
+      req.user.isAdmin
+    ) {
+      const data = await Blog.deleteOne({ _id: req.params.id });
+    } else {
+      throw new Error("You can't delete the blog!");
+    }
 
     res.status(data.deletedCount ? 204 : 404).send({
       error: !data.deletedCount,
