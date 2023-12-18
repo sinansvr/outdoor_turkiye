e918648cfef885369f7eb454d6e702bbe262e452
sinans <svrsinan06@gmail.com>
2023-12-18 13:42:42 +0300

    swagger added

--- backend/src/middlewares/logger.js
+++ backend/src/middlewares/logger.js
@@ -2,8 +2,10 @@ const morgan= require("morgan")
 
 const fs=require("node:fs")
 
+//create today log file
 const today = new Date().toISOString().split("T")[0]
 
+//add the logs to the file
 module.exports=morgan("combined",{
     stream: fs.createWriteStream(`./logs/${today}.log`)
 },{flags:"a+"});
---
+++ backend/swagger.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+const HOST = process.env?.HOST || "127.0.0.1";
+const PORT = process.env?.PORT || 8000;
+
+const swaggerAutogen = require("swagger-autogen")();
+const packageJson = require("./package.json");
+
+const document = {
+  info: {
+    version: packageJson.version,
+    title: packageJson.title,
+    description: packageJson.description,
+    termsOfService: "https://github.com/sinansvr",
+    contact: { name: packageJson.author, email: "svrsinan06@gmail.com" },
+    license: { name: packageJson.license },
+  },
+  host: `${HOST}:${PORT}`,
+  basePath: "/",
+  schemes: ["http", "https"],
+  consumes: ["application/json"],
+  produces: ["application/json"],
+  securityDefinitions: {
+    Token: {
+      type: "apiKey",
+      in: "header",
+      name: "Authorization",
+      description:
+        "Simple Token Authentication * Example: <b>Token ...tokenKey...</b>",
+    },
+  },
+  security: [{ Token: [] }]
+
+};
+
+const routes = ['./index.js']
+const outputFile = './src/configs/swagger.json'
+
+//create json file
+swaggerAutogen(outputFile, routes, document)
