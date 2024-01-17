a47cf6d818f10fe8b9fd44f0b78d87b27eeb7379
sinans <svrsinan06@gmail.com>
2024-01-17 12:24:11 +0300

    pages added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 
-import './App.css'
+// import './App.css'
 import AppRouter from './router/AppRouter'
 
 function App() {
--- frontend/src/router/AppRouter.jsx
+++ frontend/src/router/AppRouter.jsx
@@ -2,6 +2,9 @@ import { Route, BrowserRouter as Router, Routes} from 'react-router-dom'
 import Home from "../pages/Home"
 import About from '../pages/About'
 import AppBar from "../components/AppBar"
+import Login from "../pages/Login"
+import Register from '../pages/Register'
+
 
 const AppRouter = () => {
   return (
@@ -11,6 +14,10 @@ const AppRouter = () => {
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path='/about' element={<About/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
+
+
       </Routes>
     </Router>
     </>
