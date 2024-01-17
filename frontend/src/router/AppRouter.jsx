992ec3ec281b58c8ff7232ec908e9f1ff7d84b04
sinans <svrsinan06@gmail.com>
2024-01-17 13:32:22 +0300

    privaterouter

--- frontend/src/router/AppRouter.jsx
+++ frontend/src/router/AppRouter.jsx
@@ -1,25 +1,29 @@
-import { Route, BrowserRouter as Router, Routes} from 'react-router-dom'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import Home from "../pages/Home"
 import About from '../pages/About'
 import AppBar from "../components/AppBar"
 import Login from "../pages/Login"
 import Register from '../pages/Register'
+import Blogs from '../pages/Blogs'
+import PrivateRouter from './PrivateRouter'
 
 
 const AppRouter = () => {
   return (
-    <>    
-    <Router>
-      <AppBar/>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path='/about' element={<About/>}/>
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/register' element={<Register/>}/>
+    <>
+      <Router>
+        <AppBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/blogs' element={<PrivateRouter />}>
+            <Route path='/' element={<Blogs />} />
+          </Route>
 
-
-      </Routes>
-    </Router>
+        </Routes>
+      </Router>
     </>
   )
 }
