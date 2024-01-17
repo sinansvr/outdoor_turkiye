5aafbbea373566d681b23430410567c3d2922984
sinans <svrsinan06@gmail.com>
2024-01-17 10:44:42 +0300

    appbar

--- frontend/src/router/AppRouter.jsx
+++ frontend/src/router/AppRouter.jsx
@@ -1,7 +1,19 @@
+import { Route, BrowserRouter as Router, Routes} from 'react-router-dom'
+import Home from "../pages/Home"
+import About from '../pages/About'
+import AppBar from "../components/AppBar"
 
 const AppRouter = () => {
   return (
-    <div>AppRouter</div>
+    <>    
+    <Router>
+      <AppBar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+      </Routes>
+    </Router>
+    </>
   )
 }
 
