ce3a91d9211c2a884eb272059aa68097656769ae
sinans <svrsinan06@gmail.com>
2024-01-18 10:44:47 +0300

    useauthcall

---
+++ frontend/src/hooks/useAuthCall.jsx
@@ -0,0 +1,13 @@
+import {useDispatch} from 'react-redux'
+
+const useAuthCall = () => {
+const dispatch= useDispatch()
+
+const login =async ()=>{
+    
+}
+
+  return {}
+}
+
+export default useAuthCall
