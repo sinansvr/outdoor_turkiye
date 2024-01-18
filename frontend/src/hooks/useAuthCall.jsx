c4f016b9c95e183a958a2ec3ccf3c7ba179924f3
sinans <svrsinan06@gmail.com>
2024-01-19 00:07:44 +0300

    login

--- frontend/src/hooks/useAuthCall.jsx
+++ frontend/src/hooks/useAuthCall.jsx
@@ -1,13 +1,29 @@
 import {useDispatch} from 'react-redux'
+import { useNavigate } from "react-router-dom";
+import {fetchFail, fetchStart, loginSuccess} from "../features/AuthSlice"
+import axios from "axios";
+import { toastErrorNotify, toastSuccessNotify } from "../helper/ToastNotify";
 
 const useAuthCall = () => {
 const dispatch= useDispatch()
+const navigate= useNavigate()
 
-const login =async ()=>{
-    
+const login =async (userData)=>{
+    dispatch(fetchStart())
+
+    try {
+      const {data} = await axios.post(`${import.meta.env.VITE_BASE_URL}`+"users/login",userData);
+      dispatch(loginSuccess(data));
+      navigate(-1)
+      toastSuccessNotify('Successfuly logged in!')
+    } catch (error) {
+      console.log(error)
+      dispatch(fetchFail())
+      toastErrorNotify("User credentials are not correct!")
+    }
 }
 
-  return {}
+  return {login}
 }
 
 export default useAuthCall
