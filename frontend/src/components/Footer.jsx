7c6832324377344750c1bd3efc06840e3ce7f3fa
sinans <svrsinan06@gmail.com>
2024-01-22 00:22:05 +0300

    router

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import { Container } from "@mui/material"
+
+const Footer = () => {
+  return (
+    <Container maxWidth={"xl"} sx={{position:"fixed", bottom:0, backgroundColor:"#1976D2"}}>
+        Footer
+    </Container>
+  )
+}
+
+export default Footer
--- frontend/src/router/AppRouter.jsx
+++ frontend/src/router/AppRouter.jsx
@@ -11,40 +11,42 @@ import BlogDetail from '../pages/BlogDetail'
 import NewBlog from '../pages/NewBlog'
 import { Container, CssBaseline, Grid } from '@mui/material'
 import NewsBar from '../components/NewsBar'
+import Footer from '../components/Footer'
 
 
 const AppRouter = () => {
   return (
     <>
-    <CssBaseline />
-    <AppBar />
-    <Container maxWidth={'xl'}>
-    <Grid container>
-      
-      <Grid item xs={8}>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/blogs' element={<Blogs />} />
-          <Route path='/' element={<PrivateRouter />}>
-            <Route path='/profile' element={<Profile />} />
-            <Route path='/blogdetail/:id' element={<BlogDetail />} />
-            <Route path='/newblog' element={<NewBlog />} />
-          </Route>
-        </Routes>
-      </Router>
-      </Grid>
-      <Grid item xs={4} sx={{backgroundColor:"red"}}>
-        <NewsBar/>
-      </Grid>
-      
-    </Grid>
-    </Container>
+      <CssBaseline />
+      <AppBar />
+      <Container maxWidth={'xl'}>
+        <Grid container>
+
+          <Grid item xs={8}>
+            <Router>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/login' element={<Login />} />
+                <Route path='/register' element={<Register />} />
+                <Route path='/blogs' element={<Blogs />} />
+                <Route path='/' element={<PrivateRouter />}>
+                  <Route path='/profile' element={<Profile />} />
+                  <Route path='/blogdetail/:id' element={<BlogDetail />} />
+                  <Route path='/newblog' element={<NewBlog />} />
+                </Route>
+              </Routes>
+            </Router>
+          </Grid>
+          <Grid item xs={4} sx={{ backgroundColor: "red" }}>
+            <NewsBar />
+          </Grid>
+
+        </Grid>
+      </Container>
+      <Footer />
     </>
-    
+
   )
 }
 
