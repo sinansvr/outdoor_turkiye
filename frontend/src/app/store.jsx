15644d20ed8cc23df1be3e3a556bc698672461f4
sinans <svrsinan06@gmail.com>
2024-01-24 14:16:51 +0300

    blogreducer added

--- frontend/src/app/store.jsx
+++ frontend/src/app/store.jsx
@@ -1,10 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from "../features/AuthSlice"
+import blogReducer from '../features/BlogSlice';
 
 const store=configureStore({
     reducer:{
         auth:authReducer,
-    },    
+    },  blog:blogReducer
 })
 
 export default store;
